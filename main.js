547a2fd3e6875ed163f01826e3b5614e86a9df82
Justin Feldstein <92735599+justinfeldstein@users.noreply.github.com>
2022-02-09 22:08:26 -0500

    e
    e

--- main.js
+++ main.js
@@ -95,3 +95,7 @@ function showQuality() {
     document.getElementById('quality').style.display = 'block';
   }
 }
+
+function thankYou() {
+  alert("Thank You for signing up to receive the latest news about our company!");
+}
